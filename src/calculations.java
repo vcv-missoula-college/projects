63f40c96090901566511358d85d382b1f587458d
Camillia Diacon <cd143779@umontana.umt.ed>
2019-10-22 09:34:22 -0600

    Camillia add Divide

--- src/calculations.java
+++ src/calculations.java
@@ -1,12 +1,36 @@
 
 class Calculations{
 
-  // Add methods here //
+	public static void Divide (double x, double y) {
+		//Camillia Divide by double//
+		if ((y > 0) || (y < 0)) {	
+			double z = x / y;
+			System.out.println(z);
+		}
+		else {
+			System.out.println("Not valid");
+		}
+   }
+  	public static void Divide (float x, float y) {
+		//Camillia Divide by float//
+		if ((y > 0) || (y < 0)) {	
+			float z = x / y;
+			System.out.println(z);
+		}
+		else {
+			System.out.println("Not valid");
+		}
+   }
 
 
   public static void main(String[] args) {
 	Calculations myc = new Calculations();
-	// Add Unit tests here! //
+	//Camillia Unit test Double//
+	if (Divide(10000/2) == 500) 
+		System.out.println("Divide double works");
+	//Camillia unit test float//
+	if (Divide(20,0.5) == 400)
+		System.out.println("Divide float works");
   }
 }
 
