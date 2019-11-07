68dcd11b56871e8f5495129338b659844ee56f66
Valgenti <victor.valgenti@umt.edu>
2019-11-07 14:43:24 -0700

    Add entry point.

--- src/calc.java
+++ src/calc.java
@@ -1,4 +1,24 @@
 
 class MCCalculator{
-
+  public static void main(String[] args) {
+	if (args == null || args.length == 0) {
+		System.out.println("Did not designate the calculator to use! Quitting!");
+	}
+	String which_calc = args[1];
+	if (which_calc.equalsIgnoreCase("caldeen")){
+		// Add code here to starting point of implemtation
+	} else if(which_calc.equalsIgnoreCase("ccunningham")) {
+		// Add code here to starting point of implemtation
+	} else if(which_calc.equalsIgnoreCase("cdiacon")) {
+		// Add code here to starting point of implemtation
+	} else if(which_calc.equalsIgnoreCase("esargent")) {
+		// Add code here to starting point of implemtation
+	} else if(which_calc.equalsIgnoreCase("kschmaus")) {
+		// Add code here to starting point of implemtation
+	} else if(which_calc.equalsIgnoreCase("kzuleger")) {
+		// Add code here to starting point of implemtation
+	} else{
+		System.out.println("Did not select a valid calculator valid calculator! Quitting!");
+	}
+  }
 }
