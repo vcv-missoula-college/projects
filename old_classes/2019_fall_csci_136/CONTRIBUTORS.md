a23e4548f01a8d71267bd8bdf890e1e336e2d21c
Valgenti <victor.valgenti@umt.edu>
2020-01-13 12:53:05 -0700

    update repo.

---
+++ old_classes/2019_fall_csci_136/CONTRIBUTORS.md
@@ -0,0 +1,14 @@
+# Faculty
+
+* Victor C Valgenti
+
+# Students
+
+## Fall 2019
+
+* Charles Aldeen
+* Cole Cunningham
+* Camillia Diacon
+* Emerald I Sargent
+* Kajun S Schmaus
+* Kendra L Zuleger
---
+++ old_classes/2019_fall_csci_136/calc.java
@@ -0,0 +1,29 @@
+
+class MCCalculator{
+  public static void main(String[] args) {
+	if (args == null || args.length == 0) {
+		System.out.println("Did not designate the calculator to use! Quitting!");
+	}
+	String which_calc = args[0];
+	if (which_calc.equalsIgnoreCase("caldeen")){
+		// Add code here to starting point of implemtation
+	} else if(which_calc.equalsIgnoreCase("ccunningham")) {
+		// Add code here to starting point of implemtation
+		ccunningham_calc.main(args);
+	} else if(which_calc.equalsIgnoreCase("cdiacon")) {
+		// Add code here to starting point of implemtation
+	} else if(which_calc.equalsIgnoreCase("esargent")) {
+		// Add code here to starting point of implemtation
+		System.out.println("Using esargent's calculator:");
+		esargent.main(args);
+	} else if(which_calc.equalsIgnoreCase("kschmaus")) {
+		System.out.println("Using kschmaus's calculator:");
+		kschmaus1.main(args);
+	} else if(which_calc.equalsIgnoreCase("kzuleger")) {
+		// Add code here to starting point of implemtation
+    kzuleger_calculator.main(args);
+	} else{
+		System.out.println("Did not select a valid calculator valid calculator! Quitting!");
+	}
+  }
+}
