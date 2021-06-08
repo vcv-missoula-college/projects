5ddd344a029b9c544611bd68aacbde06d37b80a8
lothar243 <jeffarends@gmail.com>
2021-06-08 10:53:32 -0600

    Pacman score checker
    Use this to get feedback on your Ghost-hunting progress

---
+++ 2021_gencyber_summer_camp/resources/day1/currentScore.py
@@ -0,0 +1,33 @@
+import os
+filenames = os.listdir()
+
+
+treasure = {"cherry":100,
+            "strawberry": 300,
+            "peach": 500,
+            "apple": 700,
+            "grapes": 1000,
+            "galaxian boss": 2000,
+            "bell": 3000,
+            "key": 5000}
+
+totalscore = 0
+for filename in filenames:
+    with open(filename) as file:
+        lines = file.readlines()
+        for line in lines:
+            totalscore += treasure.get(line.strip(), 0)
+print("Your total score is {} points".format(totalscore))
+
+if totalscore == 0:
+    print("Place your ghosts in the quarantine directory to count your treasure")
+elif 0 < totalscore <= 10000:
+    print("This is a good start - there are still more ghosts out there")
+elif 10000< totalscore <= 20000:
+    print("Keep going, there's still more")
+elif 20000 < totalscore < 28000:
+    print("You've almost caught them all!")
+elif totalscore == 28000:
+    print("Congratulations! You've caught them all and achieved the highest possible score!")
+elif totalscore > 28000:
+    print("Somehow you got a greater score than is possible. Perhaps you have multiple copies of some of your ghosts")
