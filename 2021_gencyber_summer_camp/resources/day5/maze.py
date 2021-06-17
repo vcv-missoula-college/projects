78bb3536c16e186957c324fae0ff52620c60137c
Jeff Arends <jeff.arends@umontana.edu>
2021-06-16 21:52:04 -0600

    Added a countdown timer for resetting the alarm

--- 2021_gencyber_summer_camp/resources/day5/maze.py
+++ 2021_gencyber_summer_camp/resources/day5/maze.py
@@ -2,16 +2,15 @@ import gpiozero
 import time
 
 
+resetTime = 15
+
+button = gpiozero.Button(19)
+led = gpiozero.LED(26)
+pir = gpiozero.MotionSensor(13)
+
 def main():
-    button = gpiozero.Button(19)
-    led = gpiozero.LED(26)
-    pir = gpiozero.MotionSensor(13)
-
-    button.when_pressed = led.on
-    pir.when_activated = led.off
-
-    led.on()
-
+    button.when_pressed = reset
+    reset()
     try:
         while True:
             time.sleep(1)
@@ -20,5 +19,16 @@ def main():
         pir.close()
         button.close()
 
+def blinkfast():
+    led.blink(.1,.1)
+
+def reset():
+    pir.when_activated = None
+    led.blink(.3, .3)
+    time.sleep(resetTime)
+    pir.when_activated = led.off
+    led.on()
+
 if __name__ == "__main__":
     main()
+
