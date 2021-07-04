8bc29b7371d85f1b4d0fd03b5bf1d84d39a6759a
Jeff Arends <jeff.arends@umontana.edu>
2021-07-04 14:51:28 -0600

    Cleaning up

--- 2021_gencyber_summer_camp/resources/day5/distanceSensor.py
+++ 2021_gencyber_summer_camp/resources/day5/distanceSensor.py
@@ -2,6 +2,8 @@ from gpiozero import DistanceSensor
 from time import sleep
 
 dSensor = DistanceSensor(echo=24, trigger=23)
+dSensor.max_distance = 2
+
 try:
     while True:
         print(dSensor.distance)
